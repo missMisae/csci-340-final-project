865a2746a4c94e6c637dcf9b2d174dcda92a6ac7
Chandler DeLoach <deloachcd@g.cofc.edu>
2019-04-21 19:13:20 -0400

    Got 2 out of the 4 functions, making some moves here.

--- cache_functions.c
+++ cache_functions.c
@@ -5,7 +5,7 @@
 #include "cache_functions.h"
 
 #define VERBOSE
-#define NUM_POLLS 666
+#define NUM_POLLS 66
 #define NUM_ACCESSES 31072896
 
 long block_size() {
@@ -15,29 +15,12 @@ long cache_size() {
 }
 
 double speed_array_accesses_sequenced() {
-  int i;
-  long j;
-  clock_t time_anchor;
-  double access_time_total, access_time, poll_time;
-  char * slots = malloc(sizeof(char)*RAND_MAX);
-  char trash_variable;
-
-#ifdef VERBOSE
-  printf("Iterating through 666 * 6^6 values, %d times( ͡° ͜ʖ ͡°)\n", NUM_POLLS);
-#endif
-  for (i = 0; i < NUM_POLLS; i++) {
-    time_anchor = clock();
-    for (j = 0; j < NUM_ACCESSES; j++) {
-        trash_variable = slots[j];
-    }
-    poll_time = (double)(clock() - time_anchor);
-    access_time_total += poll_time / NUM_ACCESSES;
-  }
-  access_time = access_time_total / NUM_POLLS;
-  return access_time;
 }
 
 double speed_array_accesses_random() {
+}
+
+double speed_main_memory() {
   long j;
   int i, r;
   clock_t time_anchor;
@@ -46,7 +29,7 @@ double speed_array_accesses_random() {
   char trash_variable;
 
 #ifdef VERBOSE
-  printf("Iterating through 666 * 6^6 values, %d times( ͡° ͜ʖ ͡°)\n", NUM_POLLS);
+  printf("Iterating through 666 * 6^6 values, %d times ( ͡° ͜ʖ ͡°)\n", NUM_POLLS);
 #endif
   for (i = 0; i < NUM_POLLS; i++) {
     time_anchor = clock();
@@ -61,16 +44,33 @@ double speed_array_accesses_random() {
   return access_time;
 }
 
-long speed_main_memory() {
-}
+double speed_cache_memory() {
+  int i;
+  long j;
+  clock_t time_anchor;
+  double access_time_total, access_time, poll_time;
+  char * slots = malloc(sizeof(char)*RAND_MAX);
+  char trash_variable;
 
-long speed_cache_memory() {
+#ifdef VERBOSE
+  printf("Iterating through 666 * 6^6 values, %d times ( ͡° ͜ʖ ͡°)\n", NUM_POLLS);
+#endif
+  for (i = 0; i < NUM_POLLS; i++) {
+    time_anchor = clock();
+    for (j = 0; j < NUM_ACCESSES; j++) {
+        trash_variable = slots[j];
+    }
+    poll_time = (double)(clock() - time_anchor);
+    access_time_total += poll_time / NUM_ACCESSES;
+  }
+  access_time = access_time_total / NUM_POLLS;
+  return access_time;
 }
 
 int main() {
-  double access_speed_sequenced, access_speed_random;
-  access_speed_sequenced = speed_array_accesses_sequenced();
-  access_speed_random = speed_array_accesses_random();
-  printf("It takes %f to access all the slots in memory (seq).\n", access_speed_sequenced);
-  printf("It takes %f to access all the slots in memory (rand).\n", access_speed_random);
+  double access_speed_cache, access_speed_main;
+  access_speed_cache = speed_cache_memory();
+  access_speed_main = speed_main_memory();
+  printf("It takes %f to access all the slots in memory (seq).\n", access_speed_cache);
+  printf("It takes %f to access all the slots in memory (rand).\n", access_speed_main);
 }
--- cache_functions.h
+++ cache_functions.h
@@ -1,6 +1,6 @@
 long block_size();
 long cache_size();
-long speed_main_memory();
-long speed_cache_memory();
+double speed_main_memory();
+double speed_cache_memory();
 double speed_array_accesses_sequenced();
 double speed_array_accesses_random();
